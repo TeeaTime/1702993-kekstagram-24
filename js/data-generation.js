37ac3259183e84f43f9bbf0e4a1bc016a05f4574
unknown <delight_1@mail.ru>
2021-11-15 00:46:01 +0500

    Отображение миниатюр на странице

--- js/data-generation.js
+++ js/data-generation.js
@@ -25,15 +25,14 @@ const descriptions = [
 'На этой фотографии изображен древний, римский доспех'
 ];
 
-//  const SIMILAR_OFFER_COUNT = 4;
 const createComment = (i) => {
 const avatarIndex = getRandomPositiveInteger(1,6);
 
 return{
-    id: i,
-    avatar: `/img/avatar-${avatarIndex}.svg`,
-    massage: massages[getRandomPositiveInteger(0,massages.length - 1)],
-    name: names[getRandomPositiveInteger(0,names.length - 1)],
+id: i,
+avatar: `/img/avatar-${avatarIndex}.svg`,
+massage: massages[getRandomPositiveInteger(0,massages.length - 1)],
+name: names[getRandomPositiveInteger(0,names.length - 1)],
 };
 };
 
@@ -42,21 +41,21 @@ const commentsCount = getRandomPositiveInteger(0,20);
 const comments = []
 
 for (let i = 1; i <= commentsCount; i++) {
-    comments.push(createComment(i));
+comments.push(createComment(i));
 }
 return comments;
 }
 
 const createOffer = (i) => {
 return {
-    name: names[getRandomPositiveInteger(0,names.length - 1)],
-    id: i,
-    message:  massages[getRandomPositiveInteger(0,massages.length - 1)],
-    avatar: avatars[getRandomPositiveInteger(0,avatars.length - 1)],
-    likes: getRandomPositiveInteger(15,200),
-    comment: createRandomComments(),
-    url: `/photos/${i}.jpg`,
-    description: descriptions[getRandomPositiveInteger(0,descriptions.length - 1)],
+name: names[getRandomPositiveInteger(0,names.length - 1)],
+id: i,
+message:  massages[getRandomPositiveInteger(0,massages.length - 1)],
+avatar: avatars[getRandomPositiveInteger(0,avatars.length - 1)],
+likes: getRandomPositiveInteger(15,200),
+comments: createRandomComments(),
+url: `/photos/${i}.jpg`,
+description: descriptions[getRandomPositiveInteger(0,descriptions.length - 1)],
 };
 };
 
@@ -64,7 +63,7 @@ export const createffers = (count = 10) => {
 const offers = [];
 
 for (let i = 1; i <= 25; i++) {
- offers.push(createOffer(i));
+offers.push(createOffer(i));
 }
- return offers;
+return offers;
 };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import {createffers} from '/js/data-generation.js';
-
+import {renderPictures} from '/js/picture.js';
 const offers = createffers(25);
+
 
 console.log(offers);
 
 
+
+renderPictures(offers);
