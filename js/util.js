98b1e06f03cdeb79255fc109a84091b8390fdecc
unknown <delight_1@mail.ru>
2021-11-14 23:52:48 +0500

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,13 @@
+export const getRandomPositiveInteger = (a, b) => {
+const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export const checkStringLength = (string, length) => {
+  return string.length <= length;
+};
+
+
+
